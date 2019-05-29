ac09760a56105735289560660e925f34c34e53c6
ZMelisko <zmelisko@gmail.com>
2019-05-28 21:57:06 -0700

    final update 5/28/19

--- js/main.js
+++ js/main.js
@@ -1,38 +1,33 @@
 $(window).ready(function() {
+    var choisiOffset = $('#choisi-sec').offset().top;
     $('.backToTop').hide();
-    $('#choisi-sec').opacity(0);
+
+    $(window).on('resize', function () {
+        choisiOffset = $('#choisi-sec').offset().top;
+    });
 
     $(window).scroll(function() {
-    const scroll = $(window).scrollTop();
+        const scroll = $(window).scrollTop();
 
-    if (scroll >= 800) {
-        $(".nav-wrapper").addClass("scrolled").fadeOut(300);
-        $('.backToTop').fadeIn(300);
-    } else {
-        $(".nav-wrapper").removeClass("scrolled").fadeIn(300);
-        $('.backToTop').fadeOut(300);
-    }
-});
+        if (scroll >= 800) {
+            $(".nav-wrapper").addClass("scrolled").fadeOut(300);
+            $('.backToTop').fadeIn(300);
+        } else {
+            $(".nav-wrapper").removeClass("scrolled").fadeIn(300);
+            $('.backToTop').fadeOut(300);
+        }
+
+        if (scroll >= choisiOffset) {
+            $('#choisi-sec .caseStudy-des').addClass('text-scrolled')
+        }
+    });
     $('.nav-container').on('hover', function () {
         $('.work').fadeIn('dropdown-content')
     });
 });
-//
-// $('.float-up')
-//     .css('opacity', 0);
-// //
-// document.addEventListener('scroll', () => {
-//     var waypoint = new Waypoint({
-//         element: document.getElementById('choisi-sec'),
-//         handler: function() {
-//             this.destroy();
-//         },
-//         offset: 'bottom-in-view',
+
+// $('#choisi-sec').waypoint(function() {
+//     $('#choisi-sec').css({
+//         animation: "move-and-fade 2s",
 //     })
 // });
-
-$('#choisi-sec').waypoint(function() {
-    $('#choisi-sec').css({
-        animation: "move-and-fade 2s",
-    })
-});
