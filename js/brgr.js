e2048f760fb65694a8c72f8a16b0968dd3343858
ZMelisko <zmelisko@gmail.com>
2019-10-26 12:54:25 -0700

    case-study-updates

---
+++ js/brgr.js
@@ -0,0 +1,11 @@
+
+$(window).ready(function() {
+    $(window).load(function () {
+        $('#feature-img').show();
+        $('#feat-prior-text').show();
+        $('#journey-map-img').hide();
+        $('#journey-map-text').hide();
+        $('#persona-img').hide();
+        $('#persona-text').hide();
+    });
+});
